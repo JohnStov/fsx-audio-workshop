b18308c88ae9bced750478cc8e2903add5277249
John Stovin <john.d.stovin@gmail.com>
2018-02-14 08:42:52 +0000

    Sequences

--- code/AudioPlayground.fs
+++ code/AudioPlayground.fs
@@ -5,13 +5,13 @@ open System
 open NAudio.CoreAudioApi
 open System.Threading
 
-type AudioSample = float array
+type AudioSample = float seq
 
-type ArrayProvider (waveform: AudioSample) =
+type SeqProvider (waveform: AudioSample) =
     // samplerate:44.1kHz, mono
     let waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(44100, 1)
     let bytesPerSample = waveFormat.BitsPerSample / 8
-    let mutable readIndex = 0
+    let enumerator = waveform.GetEnumerator()
 
     interface IWaveProvider with
         member __.WaveFormat with get() = waveFormat
@@ -29,22 +29,34 @@ type ArrayProvider (waveform: AudioSample) =
             let nSamples = count / bytesPerSample
             for _ in [0 .. nSamples - 1] do
                 // guard against buffer overrun
-                let sample = if readIndex < waveform.Length then waveform.[readIndex] else 0.0
-                readIndex <- readIndex + 1
+                let sample = 
+                    match enumerator.MoveNext() with
+                    | true -> enumerator.Current 
+                    | false -> 0.0
                 putSample sample buffer
             // return the number of bytes written
             nSamples * bytesPerSample
 
-let makeNoise nSamples =
+let makeNoise =
     let random = Random()
     let rescale value = (value * 2.0) - 1.0
-    Array.init nSamples (fun _ -> random.NextDouble() |> rescale)
+    let gen _ = Some (random.NextDouble() |> rescale, ()) 
+    Seq.unfold gen ()
+
+let makeNoise1Sec = 
+    makeNoise |> Seq.take 44100
+
+let TWOPI = 2.0 * Math.PI
+
+let makeSine sampleRate frequency = 
+    let delta = TWOPI * frequency / float sampleRate
+    let gen theta = Some (Math.Sin theta, (theta + delta) % TWOPI)
+    Seq.unfold gen 0.0
 
 [<EntryPoint>]
 let main _ =
     let output = new WasapiOut(AudioClientShareMode.Shared, 1)
-    let noise = makeNoise 44100
-    noise |> ArrayProvider  |> output.Init
+    makeSine 44100 440.0 |> SeqProvider  |> output.Init
     output.Play ()
     Thread.Sleep 2000
     output.Stop ()
